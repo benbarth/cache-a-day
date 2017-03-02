40b406c46a97dc3b03d8d182001a0bc0dfe55f2d
Ben Barth <benbarth@users.noreply.github.com>
2017-03-02 01:26:09 -0500

    Allow upload of gpx or zip and display forcast

--- edit.php
+++ edit.php
@@ -16,12 +16,44 @@
 <?php
 
 if (isset($_POST['submit'])) {
+	$fileName = $_FILES["file"]["name"];
 	$srcFile = $_FILES["file"]["tmp_name"];
+
+	$fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+
+	if ($fileExtension == "zip") {
+		//Let's unzip the gpx
+		$tempDir = "tmp/";
+		$gpxFileName = basename($fileName, ".zip") . ".gpx";
+		$zip = new ZipArchive;
+		if ($zip->open($srcFile) === TRUE) {
+			echo $gpxFileName;
+			if ($zip->extractTo($tempDir, $gpxFileName)) {
+				$srcFile = $tempDir . $gpxFileName;
+			} else {
+				die("Unable to extract");
+			}
+			$zip->close();
+			echo $srcFile;
+		} else {
+			die("Unable to open zip");
+		}
+	} else if ($fileExtension == "gpx") {
+		// Good
+	} else {
+		die("Invalid file type");
+	}
+
+	$gpx = simplexml_load_file($srcFile) or die("Unable to parse gpx");
+
+	$user = $gpx->wpt->children("groundspeak", true)->cache->children("groundspeak", true)->logs[0]->children("groundspeak", true)->log->children("groundspeak", true)->finder;
+
 	$destFolder = "gpx/";
-	$destFile = $destFolder . "mrbarth.gpx";
-	if (move_uploaded_file($srcFile, $destFile)) {
+	$destFile = $destFolder . $user . ".gpx";
+
+	if (rename($srcFile, $destFile)) {
 		print('OK');
-		print('<script>window.location.replace("./");</script>');
+		print('<script>window.location.replace("./?user=' . $user . '");</script>');
 	} else {
 		print('NO');
 	}
--- index.php
+++ index.php
@@ -1,49 +1,95 @@
+<html>
+<head>
+	<title>cache-a-day</title>
+</head>
+<body>
 <?php
+
+$user = $_GET["user"];
+$user = preg_replace("/[^a-z\d ]/i", '', $user);
+
+if ($user == null) {
+?>
+<form action="" method="get">
+<input type="text" name="user" placeholder="User" />
+<input type="submit" />
+</form>
+<?php
+} else {
 
 $timezoneToronto = new DateTimeZone("America/Toronto");
 
-$gpxFile = "gpx/mrbarth.gpx";
+$gpxFile = "gpx/" . $user . ".gpx";
+
+if (!file_exists($gpxFile)) {
+	die("User not found");
+}
 
 $gpx = simplexml_load_file($gpxFile);
 
 echo "<h1>cache-a-day</h1>";
 
-$now = new DateTime('NOW');
+$now = new DateTime('now');
 $now->setTimezone($timezoneToronto);
-$nowShortString = $now->format("m-d");
 
-echo "Today is " . $nowShortString . ".<br/><br/>";
+$tomorrow = new DateTime('+1 day');
+$tomorrow->setTimezone($timezoneToronto);
 
-$numberOfLogs = 0;
-$found = false;
-$foundString = "";
-foreach($gpx->wpt as $cache) {
-	$logs = $cache->children('groundspeak', true)->cache->children('groundspeak', true)->logs;
-	if ($logs) {
-		foreach($logs as $log) {
-			$numberOfLogs += 1;
-			$logDateData = $log->children('groundspeak', true)->log->children('groundspeak', true)->date;
-			$logDate = DateTime::createFromFormat(DateTime::ATOM, $logDateData);
-			$logDate->setTimezone($timezoneToronto);
-			$logDateString = $logDate->format("m-d");
-			if ($logDateString == $nowShortString) {
-				$found = true;
-				$foundString = $foundString . " <a href=\"" . $cache->url . "\">" . $cache->name . "</a>";
+$day2 = new DateTime('+2 day');
+$day2->setTimezone($timezoneToronto);
+
+$day3 = new DateTime('+3 day');
+$day3->setTimezone($timezoneToronto);
+
+echo "<strong>" . $now->format("F j") . "</strong>";
+echo "<br/><br/>";
+
+function cachesFoundOnDate($gpx, $date) {
+	$foundString = "";
+	foreach($gpx->wpt as $cache) {
+		$logs = $cache->children('groundspeak', true)->cache->children('groundspeak', true)->logs;
+		if ($logs) {
+			foreach($logs as $log) {
+				$logDateData = $log->children('groundspeak', true)->log->children('groundspeak', true)->date;
+				$logDate = DateTime::createFromFormat(DateTime::ATOM, $logDateData);
+				$logDate->setTimezone($timezoneToronto);
+				$logDateString = $logDate->format("m-d");
+				$dateString = $date->format("m-d");
+				if ($logDateString == $dateString) {
+					$foundString = $foundString . ($found ? "," : "") . " <a href=\"" . $cache->url . "\">" . $cache->name . "</a>";
+					$found = true;
+				}
 			}
 		}
 	}
+	return $foundString;
 }
 
-if (!$found) {
-	echo "<img src=\"https://geocaching.com/images/logtypes/3.png\"> ";
-	echo('Go find one!');
+$foundString = cachesFoundOnDate($gpx, $now);
+$found = strlen($foundString) > 0;
+
+$imgFound = "<img src=\"img/found.png\">";
+$imgNotFound = "<img src=\"img/notfound.png\">";
+
+echo "Today ";
+if ($found) {
+	echo $imgFound;
+	echo " (You've already found" . $foundString . ".)";
 } else {
-	echo "<img src=\"https://geocaching.com/images/logtypes/2.png\"> ";
-	echo "You've already found" . $foundString;
+	echo $imgNotFound;
+	echo " Go find a cache!";
 }
-
 
 echo "<br/><br/>";
-echo "Total: " . $numberOfLogs;
+
+echo "Tomorrow " . (strlen(cachesFoundOnDate($gpx, $tomorrow)) > 0 ? $imgFound : $imgNotFound);
+echo "<br/>";
+echo $day2->format("l") . " " . (strlen(cachesFoundOnDate($gpx, $day2)) > 0 ? $imgFound : $imgNotFound);
+echo "<br/>";
+echo $day3->format("l") . " " . (strlen(cachesFoundOnDate($gpx, $day3)) > 0 ? $imgFound : $imgNotFound);
+
+}
 
 ?>
+</body>
+</html>
